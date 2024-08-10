ea35fe026e470d5547ce0f140aab8311940980bc
Saboor-Hamedi <janhamedi49@gmail.com>
2024-08-10 17:05:50 +0700

    I have fixed a lot of things, such as sidebar button, blades modifications

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,41 +14,45 @@
                     but the majority have suffered alteration in some form.
                 </p>
             </div>
-
-            {{-- Blog posts --}}
-            <div class="flex flex-wrap -mx-4">
+        </div>
+        {{-- Blog posts --}}
+        <div class="px-4 md:px-8 lg:px-16">
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-5">
                 @foreach ($posts as $post)
-                    <div class="w-full px-4 mb-10 md:w-1/2 lg:w-1/3">
-                        <div class="mb-2">
+                    <div class="shadow-md rounded-md flex flex-col">
+                        <div class="mb-2 h-48 overflow-hidden">
                             @if (!$post->image)
-                                <img src="{{ asset('storage/default/bg.jpg') }}" alt="">
+                                <img src="{{ asset('storage/default/bg.jpg') }}" alt=""
+                                    class="w-full h-full object-cover">
                             @else
-                                <img src="{{ asset('storage/postImages/' . $post->image) }}" class="w-full"
-                                    alt="Post Image">
+                                <img src="{{ asset('storage/postImages/' . $post->image) }}"
+                                    class="w-full h-full object-cover" alt="Post Image">
                             @endif
                         </div>
-                        <div>
+                        <div class="flex flex-row gap-3 justify-start sm:flex-col md:flex-row px-2">
                             <span class="block mb-2 text-xs font-semibold leading-loose text-center">
                                 Author: {{ ucfirst($post->user->name ?? 'Anonymous') }}
                             </span>
                             <span class="block mb-2 text-xs font-semibold leading-loose text-center">
                                 {{ $post->created_at->diffForHumans() ?? 'N/A' }}
                             </span>
-                            <h3>
-                                <a href="{{ route('posts.show', [$post->slug]) }}"
-                                    class="inline-block mb-4 text-xl font-semibold text-dark dark:text-white hover:text-primary sm:text-2xl lg:text-xl xl:text-2xl">
-                                    {!! $post->title ?? 'N/A' !!}
-                                </a>
-
-                            </h3>
-                            <p class="text-sm  ">
-                                {!! Str::limit($post->body, 150) ?? 'N/A' !!}
-                            </p>
-                            <div class="tags p-2">
+                        </div>
+                        <div class="flex-grow flex flex-col justify-between p-4">
+                            <div>
+                                <h3 class="mb-2">
+                                    <a href="{{ route('posts.show', [$post->slug]) }}"
+                                        class="inline-block text-xl font-semibold text-dark dark:text-white hover:text-primary sm:text-2xl lg:text-xl xl:text-2xl">
+                                        {!! $post->title ?? 'N/A' !!}
+                                    </a>
+                                </h3>
+                                <p class="text-sm">
+                                    {!! Str::limit($post->body, 150) ?? 'N/A' !!}
+                                </p>
+                            </div>
+                            <div class="tags mt-2">
                                 @foreach ($post->tags as $tag)
                                     <span
-                                        class="text-sm font-medium me-2 px-2.5 
-                                                py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
+                                        class="text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
                                         #{{ $tag->name }}
                                     </span>
                                 @endforeach
@@ -58,12 +62,12 @@
                 @endforeach
             </div>
             {{-- Pagination --}}
-            <div class="pagination-wrapper mt-10">
-                {{-- {{ $posts->links('vendor.pagination.tailwind') }} --}}
+            <div class="px-4 md:px-8 lg:px-16 mt-10">
                 {{ $posts->links() }}
             </div>
-            {{-- End blog posts --}}
         </div>
+
+        {{-- End blog posts --}}
     </section>
     <x-footer></x-footer>
 </x-layout>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -38,8 +38,7 @@
                     </div>
                 </figure>
                 <div class="inline-block p-2">
-                    <h1
-                        class="mb-4 text-4xl font-extrabold leading-none tracking-tight  md:text-5xl lg:text-6xl ">
+                    <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight  md:text-5xl lg:text-6xl ">
                         {!! Str::ucfirst($post->title) !!}
                     </h1>
                     {!! $post->body !!}
@@ -57,65 +56,64 @@
             </article>
 
         </div>
-    </main>
-    <!-- ====== Blog Section Start -->
 
-    <section class="pb-10 lg:pb-1  dark__mode">
-        <div class="flex flex-wrap justify-center">
-            <div class="w-full px-4">
-                <div class="mx-auto mb-12 max-w-[800px] text-center lg:mb-20">
-                    <h2 class="text-dark mb-3 text-lg leading-[1.2] font-bold sm:text-4xl md:text-[30px]">
-                        Discover the Insights Behind Every Post
-                    </h2>
-                    <p class="text-base text-body-color">
-                        Welcome to our blog! Each post is crafted with passion to bring you the latest insights and
-                        trends. Our dedicated authors share their knowledge to keep you informed and inspired. Join our
-                        community and elevate your understanding with our expertly written content.
-                    </p>
+        <!-- ====== Blog Section Start -->
+
+        <section class="pb-10 lg:pb-1  dark__mode">
+            <div class="flex flex-wrap justify-center">
+                <div class="w-full px-4">
+                    <div class="mx-auto mb-12 max-w-[800px] text-center lg:mb-20">
+                        <h2 class="text-dark mb-3 text-lg leading-[1.2] font-bold sm:text-4xl md:text-[30px]">
+                            Discover the Insights Behind Every Post
+                        </h2>
+                        <p class="text-base text-body-color">
+                            Welcome to our blog! Each post is crafted with passion to bring you the latest insights and
+                            trends. Our dedicated authors share their knowledge to keep you informed and inspired. Join
+                            our
+                            community and elevate your understanding with our expertly written content.
+                        </p>
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
 
-    <section class="pb-10 lg:pb-20 dark__mode">
+        <section class="flex flex-col px-10 bg-gray-100 dark__mode">
 
-        <div class="container mx-auto px-4">
-            <div class="flex flex-wrap -mx-4">
+            <div class="flex flex-wrap justify-center gap-5">
                 @foreach ($otherPosts as $otherPost)
-                    <div class="w-full px-4 md:w-1/2 lg:w-1/3">
+                    <div class="w-full shadow-md rounded-md flex flex-col">
                         <div class="w-full mb-10">
-                            <div class="mb-8 overflow-hidden rounded">
-
-                                {{-- <img src="https://cdn.tailgrids.com/2.0/image/application/images/blogs/blog-01/image-01.jpg"
-                                    alt="image" class="w-full" /> --}}
+                            <div class="mb-2 h-48 overflow-hidden">
                                 @if (!$otherPost->image)
-                                    <img src="{{ asset('storage/default/bg.jpg') }}" alt="">
+                                    <img src="{{ asset('storage/default/bg.jpg') }}" alt=""
+                                        class="w-full h-full object-cover">
                                 @else
-                                    <img src="{{ asset('storage/postImages/' . $otherPost->image) }}" class="w-full"
-                                        alt="Post Image">
+                                    <img src="{{ asset('storage/postImages/' . $otherPost->image) }}"
+                                        class="w-full h-full object-cover" alt="Post Image">
                                 @endif
                             </div>
-                            <div>
+                            <div class="flex flex-row ml-2 gap-3 justify-start sm:flex-col md:flex-row">
                                 <span class="block mb-2 text-xs font-semibold leading-loose text-center">
                                     Author: {{ ucfirst($otherPost->user->name ?? 'Anonymous') }}
                                 </span>
                                 <span class="block mb-2 text-xs font-semibold leading-loose text-center">
                                     {{ $otherPost->created_at->diffForHumans() ?? 'N/A' }}
-
                                 </span>
-                                <h3>
-                                    <a href="{{ route('posts.show', [$otherPost->slug]) }}"
-                                        class="block mb-4 text-xl font-semibold text-dark dark:text-black hover:text-primary sm:text-2xl lg:text-xl xl:text-2xl">
-                                        {{ $otherPost->title ?? 'N/A' }}
-
-                                    </a>
-
-                                </h3>
-                                <p class="text-base text-body-color dark:text-dark-6">
-                                    {!! Str::limit($otherPost->body, 50) ?? 'N/A' !!}
-                                </p>
                             </div>
-                             <div class="tags p-2">
+                            <div class="flex-grow flex flex-col justify-between p-4">
+                                <div>
+                                    <h3>
+                                        <a href="{{ route('posts.show', [$otherPost->slug]) }}"
+                                            class="block mb-4 text-xl font-semibold text-dark dark:text-black hover:text-primary sm:text-2xl lg:text-xl xl:text-2xl">
+                                            {{ $otherPost->title ?? 'N/A' }}
+                                        </a>
+                                    </h3>
+                                    <p class="text-base text-body-color dark:text-dark-6">
+                                        {!! Str::limit($otherPost->body, 50) ?? 'N/A' !!}
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="tags p-2">
                                 @foreach ($post->tags as $tag)
                                     <span
                                         class="text-sm font-medium me-2 px-2.5 
@@ -132,7 +130,6 @@
                 {{-- {{ $otherPosts->links('vendor.pagination.tailwind') }} --}}
                 {{ $otherPosts->links() }}
             </div>
-        </div>
-    </section>
-
+        </section>
+    </main>
 </x-layout>
